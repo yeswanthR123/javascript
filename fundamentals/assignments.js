e86119f69caf1a4939628e4579dc20b039360242
yeswanth123h <yeswanthvarashini@gmail.com>
2025-04-01 23:20:00 +0530

    first commit

---
+++ fundamentals/assignments.js
@@ -0,0 +1,12 @@
+const country = "India";
+const continent = "Asia";
+let population = 103;
+console.log("My country is",country,"continent is",continent,"population is",population);
+
+
+const isIsland = false;
+const language = "Tamil";
+console.log(typeof(isIsland));
+console.log(typeof(language));
+console.log(typeof(country));
+console.log(typeof(population));
---
+++ fundamentals/script.js
@@ -0,0 +1,87 @@
+// //javascript is a object-oriented , multi-paradigm programming language
+// //From ES6 to later version are called modern web
+// // To link the javascript file to html use <script src = ""> tag
+
+// // values and variables
+
+// /*let firstName = "yami"; // declaration of variable 
+
+// // variable is like a box to hold the value
+
+// console.log(firstName);
+// console.log(firstName);
+
+// //variable can only start with letters,underscores,doller symbol
+// let $top = "Black Bull";
+
+// console.log(firstName,"captain of",$top);*/
+
+// // Naming convensions
+// // constant are in UPPER case
+// // variable start with lowercase
+// // variables names should be more descriptive
+
+
+
+// // Datatypes
+// // value can be:
+// //  1.primitive
+// //  2.objects
+
+// // 7 primitive data types
+// // Numbers,string,boolean,undefined,null,BigInt,symbol
+
+// /*let age = 21;
+// console.log(age);
+// //typeof operator is used to find the datatype of the variable
+// console.log(typeof age);
+// console.log(typeof "hello");
+// console.log(typeof 33.3);
+// console.log(typeof true);
+// console.log(typeof null);*/ // it should return null, but it return object which is error
+
+
+// //let = variable which can the changed later or it is mutable
+// //var = old version of the let to declare variable
+// // const = vairable cannot be change after it is assigned or it is immutable
+
+// // const dateOfBirth = 2003;
+// //const name; // const variable must be initialized
+
+// // var firstVariable = "legacy";
+
+// // lastName = "Ragu"; // we can declare variable like this also, but it is not recommanded
+
+// const firstName = "Yami";
+// const age = 30;
+
+// console.log(firstName,"age",age**2); // ** to perform exponent of a number 
+// const lastName = "sukehiro";
+// console.log(firstName+" "+lastName);
+// let teamSize = 10;
+// console.log(firstName,lastName,"team size is",teamSize);
+// teamSize+=1;
+// console.log(teamSize);
+// teamSize--;
+// console.log(teamSize);
+// teamSize++;
+
+// //comparison operators: <,>,<=,>=
+
+
+//operator precedence;
+/* 
+1.()
+2.++,--
+3.**
+4.*,/
+5.+,-
+*/
+
+console.log(2**1**3); // here the execution start from the right to left so, 2^(1^3)
+console.log(2+4+4-2);// right to left because +,- has the same level of precedence
+console.log(2*4+3+2+2);// * has the higher precedence then the +,- so it will be executed first follow by addition and subtraction
+
+
+
+
