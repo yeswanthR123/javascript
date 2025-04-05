4b86a7248ba27453c9488a0fdc420b3a2d890910
yeswanth123h <yeswanthvarashini@gmail.com>
2025-04-05 22:53:00 +0530

    created the modal window

--- model window/script.js
+++ model window/script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+//querySelector is uesd select only the first element
+// querySelectorAll is used to select all the element with same class;
+const closeModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModel = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModel);
+}
+
+btnCloseModal.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
